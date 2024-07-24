95483392469c81615957aef2823b489435db4b22
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2024-07-23 21:41:43 -0700

    Player horizontal movement working

--- Assets/Code/MonoBehaviours/PlayerController.cs
+++ Assets/Code/MonoBehaviours/PlayerController.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Code.MonoBehaviours {
     public class PlayerController : MonoBehaviour {
+        [SerializeField] private float _moveSpeed = 5f;
+        private Rigidbody2D _rb;
+        private InputAction _moveAction;
+        
        private void Start() {
-           Debug.Log("PlayerController Start");
-       } 
+           _rb = GetComponent<Rigidbody2D>();
+           _moveAction = InputSystem.actions.FindAction("HorizontalMove");
+       }
+
+       private void FixedUpdate() {
+           float horizontalMoveInput = _moveAction.ReadValue<float>();
+           _rb.velocity = new Vector2(horizontalMoveInput * _moveSpeed, _rb.velocity.y);
+       }
     }
 }
