c4f7685eb16d1836d18b3d6ec08b39b4774818e5
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2024-07-23 21:09:21 -0700

    Initial commit

---
+++ Assets/Code/Data/GameStatesManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Data {
+    public class GameState {
+        private static GameState _instance;
+
+        public static GameState Instance {
+            get {
+                if (_instance == null) {
+                    Debug.Log("No GameStatesManager instance found in current scene");
+                }
+
+                return _instance;
+            }
+        }
+        
+        private readonly List<Func<GameStates>> _subscribers = new List<Func<GameStates>>();
+        public void Subscribe(Func<GameStates> subscriber) {
+            _subscribers.Add(subscriber);
+        }
+
+        private GameStates _state;
+        public GameStates State {
+            get => _state;
+            set {
+                _state = value;
+                foreach (var subscriber in _subscribers) {
+                    subscriber.Invoke();
+                }
+            }
+        }
+    }
+
+    public enum GameStates {
+        MainMenu,
+        InGame,
+        GameOver
+    }
+}
---
+++ Assets/Code/MonoBehaviours/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Code.MonoBehaviours {
+    public class PlayerController : MonoBehaviour {
+       private void Start() {
+           Debug.Log("PlayerController Start");
+       } 
+    }
+}
